f09f3ed65ff8317073af0dee1abe176e8e844462
Sangyoon Kwon <112511940+skwon23@users.noreply.github.com>
2022-11-30 02:07:57 +0000

    controller, hero, monster, character

--- main.py
+++ main.py
@@ -13,75 +13,9 @@ if __name__ == '__main__':
     main()
 
 
-class Character:
-    def __init__(self, name, health, power):
-        self.name = name
-        self.health = health
-        self.power = power
 
-    def alive(self):
-        return self.health > 0
 
-    def attack(self, enemy):
-        if not self.alive():
-            return
-        print("%s attacks %s" % (self.name, enemy.name))
-        enemy.receive_damage(self.power)
-        self.print_status()
 
-    def receive_damage(self, points):
-        self.health -= points
-        print("%s received %d damage." % (self.name, points))
-        if self.health <= 0:
-            print("%s is dead." % self.name)
 
-    def print_status(self):
-        print("%s has %d health and %d power." % (self.name, self.health, self.power))
 
-class Hero(Character):
-    def __init__(self, name, health, power):
-        super().__init__(name, health, power)
 
-    def receive_damage(self, points):
-        self.health -= points
-        print("%s received %d damage." % (self.name, points))
-        if self.health <= 0:
-            print("%s is dead." % self.name)
-
-    def attack(self, enemy):
-        double_power = self.power * 2
-        if not self.alive():
-            return
-        print("%s attacks %s" % (self.name, enemy.name))
-        enemy.receive_damage(double_power)
-        self.print_status()
-
-    def print_status(self):
-        print("%s has %d health and %d power." % (self.name, self.health, self.power))
-
-    def restore(self):
-        self.health = 10
-        print("%s's health is restored to %d!" % (self.name, self.health))
-        self.print_status()
-
-class Goblin(Character):
-    def __init__(self, name, health, power):
-        super().__init__(name, health, power)
-
-    def receive_damage(self, points):
-        self.health -= points
-        print("%s received %d damage." % (self.name, points))
-        if self.health <= 0:
-            print("%s is dead." % self.name)
-            return True
-
-class Zombie(Character):
-    def __init__(self, name, health, power):
-        super().__init__(name, health, power)
-
-    def receive_damage(self, points):
-        self.health -= points
-        print("%s received %d damage." % (self.name, points))
-        if self.health <= 0:
-            print("%s is dead." % self.name)
-            return True
--- src/Monster.py
+++
@@ -1,7 +0,0 @@
-class Monster:
-  def __init__(self,x,y,image_monster):
-    self.location = (x,y)
-    self.image = image_monster
-
-  def damage(self,atk):
-    self.atk = atk
