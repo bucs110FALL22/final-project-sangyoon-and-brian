3e7f3cbbb5c49e2d9eca44de9fec6c0fd71b0d75
BrianLu0101 <112454837+BrianLu0101@users.noreply.github.com>
2022-11-30 21:48:13 -0500

    Delete plant.py

--- src/plant.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from .settings import *
-
-class Plant(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
-        super().__init__(groups)
-        self.image = pygame.image.load("../assets/plant.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
